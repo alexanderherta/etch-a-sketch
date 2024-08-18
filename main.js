98d87137ab3f0d2cfd5bddc0e06f3ee5f6bd3606
Alexander Herta <alexanderherta2002@proton.me>
2024-08-18 13:47:08 -0400

    Allow the grid size to change

--- main.js
+++ main.js
@@ -1,21 +1,27 @@
 let container = document.querySelector(".container");
 
-function createGrid() {
-  for(row = 1; row < 17; row++) {
+function createGrid(gridDimensions) {
+  let gridPixelSize = 800 / gridDimensions;
+
+  for(row = 1; row < (gridDimensions + 1); row++) {
     let column = 1;
     let square = document.createElement("div");
     square.id = "square-" + row + "-" + column;
     square.className = "squares";
+    square.style.height = `${gridPixelSize}px`;
+    square.style.width = `${gridPixelSize}px`;
     container.appendChild(square);
-    for(column = 2; column < 17; column++) {
+    for(column = 2; column < (gridDimensions + 1); column++) {
       let square = document.createElement("div");
       square.id = "square-" + row + "-" + column;
       square.className = "squares";
+      square.style.height = `${gridPixelSize}px`;
+      square.style.width = `${gridPixelSize}px`;
       container.appendChild(square);
     }
   }
 }
-createGrid();
+createGrid(22);
 
 let squares = document.querySelectorAll(".squares");
 
