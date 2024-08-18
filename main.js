8cad9814554be1b8f93feca7962d5ceb23fbb67c
Alexander Herta <alexanderherta2002@proton.me>
2024-08-18 11:47:54 -0400

    Add a hovering effect that changes the div colors

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
 let container = document.querySelector(".container");
 
-for(row = 1; row < 17; row++) {
-  let column = 1;
-  let square = document.createElement("div");
-  square.id = "square-" + row + "-" + column;
-  square.className = "squares";
-  container.appendChild(square);
-  for(column = 2; column < 17; column++) {
+function createGrid() {
+  for(row = 1; row < 17; row++) {
+    let column = 1;
     let square = document.createElement("div");
     square.id = "square-" + row + "-" + column;
     square.className = "squares";
     container.appendChild(square);
+    for(column = 2; column < 17; column++) {
+      let square = document.createElement("div");
+      square.id = "square-" + row + "-" + column;
+      square.className = "squares";
+      container.appendChild(square);
+    }
   }
 }
+createGrid();
+
+let squares = document.querySelectorAll(".squares");
+
+squares.forEach((square) => {
+  square.addEventListener("mouseover", () => {
+    square.style.backgroundColor = "black";
+  });
+});
